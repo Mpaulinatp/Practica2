62d486ad1b9046bbeab174f5148f419c54eeee98
isabella <danna.garcia@udea.edu.co>
2023-11-30 01:15:22 -0500

    Punto 6.2

--- inventario/library/urls.py
+++ inventario/library/urls.py
@@ -9,6 +9,10 @@ urlpatterns=[
     path('inventario/editar/<int:id>', views.editarIn, name='editar'),
     path('borrar/<int:id>', views.borrarIn, name='borrar'),
     path('responsable/crearRe', views.crearRe, name='crearRe'),
+    path('consultar/activos', views.archivos, name='archivos'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    
     
 
 ]
--- inventario/library/views.py
+++ inventario/library/views.py
@@ -4,7 +4,35 @@ from .models import Invent
 from .models import Respons
 from .forms import ResponsForm
 from .forms import InventForm
+from .forms import UserLoginForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
+def login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                next_url = request.GET.get('next', '') 
+                return redirect('inicio')
+            else:
+                return render(request=request, template_name="registro/login.html", context={'form': form})
+        else:
+            for key, error in list(form.errors.items()):
+                messages.error(request, error)
+    form = UserLoginForm()
+    return render(request=request, template_name="registro/login.html", context={'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login') 
+
+@login_required
 def inicio(request):
     equipos = Invent.objects.all()
     total_equipos = equipos.count()
@@ -29,17 +57,33 @@ def inicio(request):
         'porcentaje_sistemas': porcentaje_sistemas,
     })
 
-
-
+@login_required
 def responsables(request):
     responsables = Respons.objects.all()
     return render(request, 'responsables/listar.html', {'responsables':responsables})
 
-
+@login_required
 def inventario(request):
     equipos = Invent.objects.all()
     return render(request, 'inventario/listarIn.html', { 'equipos':equipos})
 
+@login_required
+def archivos(request):
+    equipos_biomedica = Invent.objects.filter(area='Biomédica')
+    equipos_infraestructura = Invent.objects.filter(area='Infraestructura')
+    equipos_sistemas = Invent.objects.filter(area='Sistemas')
+    equipos = Invent.objects.all()
+
+    context = {
+        'equipos_biomedica': equipos_biomedica,
+        'equipos_infraestructura': equipos_infraestructura,
+        'equipos_sistemas': equipos_sistemas,
+        'equipos':equipos
+    }
+   
+    return render(request, 'consultar/activos.html', context)
+
+@login_required
 def crearIn(request):
     infoActivo=InventForm(request.POST or None)
     if infoActivo.is_valid():
@@ -48,11 +92,13 @@ def crearIn(request):
     responsables = Respons.objects.all()
     return render(request, 'inventario/crearIn.html', {'responsables':responsables})
 
+@login_required
 def borrarIn(request, id):
     equipo =Invent.objects.get(id=id)
     equipo.delete()
     return redirect('inventario')
 
+@login_required
 def  editarIn(request,id):
     inventario=Invent.objects.get(id=id)
     formInvent=InventForm(request.POST or None , instance=inventario)
@@ -62,6 +108,7 @@ def  editarIn(request,id):
         return redirect('inventario')
     return render(request, 'inventario/editarIn.html', {'formInvent':formInvent, 'responsabless':responsabless})
 
+@login_required
 def crearRe(request):
     infoResponsable=ResponsForm(request.POST or None)
     if infoResponsable.is_valid():
